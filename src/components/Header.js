3f047fe9ee1a4ad4c49c599ce14beec3381f6ec5
Christian <chritiangabriel4@gmail.com>
2023-06-12 20:31:09 -0300

    Header modification

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className="headerContainer">
-            <p>Header</p>
-        </div>
+        <header className="headerContainer">
+            <div className='divContainer'>
+                <p>Header</p>
+            </div>
+        </header>
     )       
 }
 
