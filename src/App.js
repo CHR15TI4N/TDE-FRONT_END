160b121a17b0be911fe4baef51c517c71e6cd31a
Christian <chritiangabriel4@gmail.com>
2023-06-13 21:20:12 -0300

    auteration

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ function App() {
       </header>
       <body>
         <UserInfo
-        name={"Christian"}
-        birthday={"30/04/2004"}
-        email={"teste@email.com"}
+          name={"Christian"}
+          birthday={"30/04/2004"}
+          email={"teste@email.com"}
         />
         <UserInfo
-        color={"#366"}
-        name={"Christian2"}
-        birthday={"30/04/2004"}
-        email={"teste2@email.com"}
+          color={"#366"}
+          name={"Christian2"}
+          birthday={"30/04/2004"}
+          email={"teste2@email.com"}
         />
         <UserInfo
-        color={"#e24"}
-        name={"Christian3"}
-        birthday={"30/04/2004"}
-        email={"teste3@email.com"}
+          color={"#e24"}
+          name={"Christian3"}
+          birthday={"30/04/2004"}
+          email={"teste3@email.com"}
         />
       </body>
     </div>
